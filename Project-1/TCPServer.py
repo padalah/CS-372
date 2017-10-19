8a913617e3b9b6bb4043b5c059208d8da900186c
Jon-Eric Cook <mrjec@protonmail.com>
2017-10-19 12:40:06 -0700

    starting point

--- Project-1/TCPServer.py
+++ Project-1/TCPServer.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
+
 import socket
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 500
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
+conn, addr = s.accept()
 
-print 'The server is ready to receive'
 while 1:
-    conn, addr = s.accept()
-    data = conn.recv(BUFFER_SIZE)
-    capData = data.upper()
-    conn.send(capData)
-    conn.close()
+    received = conn.recv(BUFFER_SIZE)
+    if received == "\quit": break
+    print "Client:", received
+    sent = raw_input("Server: ")
+    conn.send(sent)
+    if sent == "\quit": break
+conn.close()
 
 #https://wiki.python.org/moin/TcpCommunication
