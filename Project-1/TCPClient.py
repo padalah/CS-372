b5548c59455a46d59b145b1241f99e7da6355d49
Jon-Eric Cook <mrjec@protonmail.com>
2017-10-19 14:56:40 -0700

    progress

--- Project-1/TCPClient.py
+++ Project-1/TCPClient.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python
 
 import socket
+import os
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+os.system('clear')
 BUFFER_SIZE = 500
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+print "******* Server Info *******"
+TCP_IP = raw_input("Hostname: ")
+TCP_PORT = int(raw_input("Port Number: "))
+print "****************************"
+print "\n"
 
+print "******* Client Setup *******"
+handle = raw_input("User handle: ")
+print "****************************"
+print "\n"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print "=> Client has been created..."
+s.connect((TCP_IP, TCP_PORT))
+print "=> Connected to the server..."
+print "=> You may begin messaging..."
+print "\n"
+
+print "-Messages-"
 while 1:
-    sent = raw_input("Client: ")
-    s.send(sent)
-    if sent == "\quit": break
+    sent = raw_input(handle + ": ")
+    s.send(handle + ": " + sent)
+    if "\quit" in sent: break
     received = s.recv(BUFFER_SIZE)
-    if received == "\quit": break
-    print "Server:", received
+    if "\quit" in received: break
+    print received
 s.close()
 
 #https://wiki.python.org/moin/TcpCommunication
--- Project-1/TCPServer.py
+++ Project-1/TCPServer.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python
 
 import socket
+import os
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
 BUFFER_SIZE = 500
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-conn, addr = s.accept()
-
 while 1:
-    received = conn.recv(BUFFER_SIZE)
-    if received == "\quit": break
-    print "Client:", received
-    sent = raw_input("Server: ")
-    conn.send(sent)
-    if sent == "\quit": break
-conn.close()
+    os.system('clear')
+    print "******* Server Setup *******"
+    TCP_IP = raw_input("Hostname: ")
+    TCP_PORT = int(raw_input("Port Number: "))
+    handle = raw_input("User handle: ")
+    print "****************************"
+    print "\n"
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+    print "=> Server has been created..."
+    s.listen(1)
+    print "=> Listening for clients..."
+    conn, addr = s.accept()
+    print "=> Connected to a client..."
+    print "       address:",addr[0]
+    print "       port number:",addr[1]
+    print "=> Awaiting message from client..."
+    print "\n"
+    print "-Messages-"
 
-#https://wiki.python.org/moin/TcpCommunication
+
+    while 1:
+        received = conn.recv(BUFFER_SIZE)
+        if "\quit" in received: break
+        print received
+        sent = raw_input(handle + ": ")
+        conn.send(handle + ": " + sent)
+        if "\quit" in sent: break
+    conn.close()
+
+# https://wiki.python.org/moin/TcpCommunication
